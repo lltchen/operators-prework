8b03f254c6bf52808e7ba5296a8250ed2385b0ee
lltchen <lltchen1@gmail.com>
2018-12-28 07:02:44 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,17 @@
 def unsafe?(speed)
 	if speed > 60
-		return true
+		return "true"
 	elsif speed < 40
-		return true
+		return "true"
 	elsif 40 < speed > 60
-		return false
+		return "false"
 	end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 ? return true : return false
-	speed < 40 ? return true : return false
-	40 < speed > 60 ? return false : return true
+	speed > 60 ? return "true" : return "false"
+	speed < 40 ? return "true" : return "false"
+	40 < speed > 60 ? return "false" : return "true"
 end
