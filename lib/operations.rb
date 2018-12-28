650731df9ba8e0b2f8dcda3debedf0a7bc47474c
lltchen <lltchen1@gmail.com>
2018-12-28 07:04:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,7 +11,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 ? return true : return false
-	speed < 40 ? return true : return false
-	40 < speed > 60 ? return false : return true
+	speed > 60 ? return true: return false
+	speed < 40 ? return true: return false
+	40 < speed > 60 ? return false: return true
 end
