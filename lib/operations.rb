eaa02358533bc567136e6b8b883f4794989c6f2d
lltchen <lltchen1@gmail.com>
2018-12-28 07:00:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+	if speed > 60
+		return true
+	elsif speed < 40
+		return true
+	elsif 40 < speed > 60
+		return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 ? return true : return false
+	speed < 40 ? return true : return false
+	40 < speed > 60 ? return false : return true
 end
-	
-
-
